f1fccea534abe5b174381bc189b492549c7b3cd9
Jonathon Blonchek <jblonchek@gmail.com>
2013-12-02 17:09:34 -0500

    rename portfolio entry.

--- StockPortfolio/src/Portfolio.java
+++ StockPortfolio/src/Portfolio.java
@@ -1,72 +1,49 @@
 import java.util.ArrayList;
 
-
-
 public class Portfolio {
 
+	private static Portfolio instance;
 
-private static Portfolio instance; 
+	private ArrayList<PortfolioEntry> entryList;
 
-private ArrayList<PortfolioEntries> EntryList; 
+	private Portfolio() {
+		entryList = new ArrayList<PortfolioEntry> ();
+	}
 
+	
+	public static Portfolio getInstance()
 
-private Portfolio (){
+	{
+		if (instance == null)
+			instance = new Portfolio();
 
+		return instance;
+	}
 
-}
+	public void display() {
 
+	}
 
-public static Portfolio getInstance()
+	public void update() {
+		for (int i = 0; i < entryList.size(); i++){
+			entryList.get(i).update();
+		}
+	}
 
-{
+	public boolean addStock() {
 
-if (instance == null)
+		return false;
 
-instance = new Portfolio();
+	}
 
+	public boolean removeStock() {
 
+		return false;
 
-return instance;
+	}
 
-}
+	public static void main(String[] args) {
 
-
-public void display() {
-
-
-}
-
-
-public void update(){
-
-
-}
-
-
-public boolean addStock(){
-
-return false;
-
-
-}
-
-
-public boolean removeStock(){
-
-return false;
+	}
 
 }
-
-
-public static void main(String[] args) {
-
-
-
-
-
-}
-
-
-
-}
-
---
+++ StockPortfolio/src/PortfolioEntry.java
@@ -0,0 +1,9 @@
+
+public class PortfolioEntry {
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
